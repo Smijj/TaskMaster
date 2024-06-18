cd581440280119ab0fab046ba43653c140e545ea
Declan Smyth <declan.smyth@hotmail.com>
2024-06-18 17:39:56 +0930

    Setup the Barrier to clamp to the bottom half of its circular rotation

--- U.BarrierToEntry/Assets/DSmyth/Scripts/BarrierModule/BarrierCtrl.cs
+++ U.BarrierToEntry/Assets/DSmyth/Scripts/BarrierModule/BarrierCtrl.cs
@@ -9,16 +9,36 @@ namespace DSmyth.BarrierModule
         [SerializeField] private RectTransform m_PivotPoint;
         [SerializeField] private RectTransform m_Barrier;
         [SerializeField] private float m_RotationSpeed = 20;
-
+        [SerializeField] private float m_RotationRadius = 155;
+        
+        [SerializeField] private float m_BarrierAngle;
 
         private void Start() {
             Cursor.lockState = CursorLockMode.Locked;
-            
+
+
+            m_Barrier.localPosition = new Vector3(0, -m_RotationRadius, 0);
         }
 
         private void Update() {
-            float mouseInputDirection = Input.GetAxis("Mouse X");
-            m_Barrier.RotateAround(m_PivotPoint.position, Vector3.forward, mouseInputDirection * m_RotationSpeed * Time.deltaTime);
+
+            m_BarrierAngle = m_Barrier.localEulerAngles.z;
+
+            // Keep the shield clamped to below the pivot point (i.e. 180 degrees of freedom)
+            if (m_Barrier.localPosition.y <= 0) {
+                float mouseInputDirection = Input.GetAxis("Mouse X");
+                m_Barrier.RotateAround(m_PivotPoint.position, Vector3.forward, mouseInputDirection * m_RotationSpeed * Time.deltaTime);
+            } else {
+                // Move the shield back into the clamp range
+                if (m_Barrier.localPosition.x < 0) {
+                    m_Barrier.localPosition = new Vector3(-m_RotationRadius, 0, 0);
+                    m_Barrier.localEulerAngles = new Vector3(0, 0, -90);
+                } else {
+                    m_Barrier.localPosition = new Vector3(m_RotationRadius, 0, 0);
+                    m_Barrier.localEulerAngles = new Vector3(0, 0, 90);
+                }
+            }
+
         }
     }
 }
