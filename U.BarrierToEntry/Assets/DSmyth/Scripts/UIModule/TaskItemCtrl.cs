6b2005f07e201a3dd109f4b3e2c77e363a8804d7
Declan Smyth <declan.smyth@hotmail.com>
2024-07-03 01:26:44 +0930

    Added most the final art to the game. I also tweaked the Tasklist Items Crossout line to use a slider instead of directly editing the image's fill amount.

--- U.BarrierToEntry/Assets/DSmyth/Scripts/UIModule/TaskItemCtrl.cs
+++ U.BarrierToEntry/Assets/DSmyth/Scripts/UIModule/TaskItemCtrl.cs
@@ -8,20 +8,23 @@ namespace DSmyth.UIModule
 {
     public class TaskItemCtrl : MonoBehaviour
     {
-        [SerializeField] private Image m_ImgCrossLine;
+        [SerializeField] private Slider m_ImgCrossLine;
         [SerializeField] private TMP_Text m_TxtItemName;
 
 
         public void InitTask(string taskName) {
             LeanTween.cancel(m_CrossOffAnimID);
-            if (m_ImgCrossLine) m_ImgCrossLine.fillAmount = 0;
+            if (m_ImgCrossLine) {
+                m_ImgCrossLine.value = 0;
+                m_ImgCrossLine.maxValue = 1;
+            }
             if (m_TxtItemName) m_TxtItemName.text = "- " + taskName;
         }
 
         private int m_CrossOffAnimID = 0;
         public LTDescr CrossOffItemAnim(float animTime = 0.25f) {
             LeanTween.cancel(m_CrossOffAnimID);
-            LTDescr tween = LeanTween.value(gameObject, x => m_ImgCrossLine.fillAmount = x, 0, 1, animTime);
+            LTDescr tween = LeanTween.value(gameObject, x => m_ImgCrossLine.value = x, 0, 1, animTime);
             m_CrossOffAnimID = tween.uniqueId;
             return tween;
         }
